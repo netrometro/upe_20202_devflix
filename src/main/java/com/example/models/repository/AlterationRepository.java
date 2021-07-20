92f878564b2afa4011a43abaadc307ca55c9209e
Lucas Henrique de Assis Monteiro <lucasmonte2011@gmail.com>
2021-07-20 11:18:00 -0300

    Criação do AlterationRepository para a classe AlterationModel.

---
+++ src/main/java/com/example/models/repository/AlterationRepository.java
@@ -0,0 +1,26 @@
+package com.example.models.repository;
+
+import com.example.models.AlterationModel;
+import com.example.models.interfaces.IRepository;
+
+public class AlterationRepository implements IRepository<AlterationModel> {
+
+  @Override
+  public AlterationModel insert(AlterationModel data) throws Exception {
+    return null;
+  }
+
+  @Override
+  public AlterationModel fetch() throws Exception {
+    return null;
+  }
+
+  @Override
+  public void update(AlterationModel data) throws Exception {
+  }
+
+  @Override
+  public void delete(int id) throws Exception {
+  }
+  
+}
