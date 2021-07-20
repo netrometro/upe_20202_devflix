e4b7c035fc7a44679b336cb61f96cc55d0545211
Luiz Gustavo <luiz.matias@upe.br>
2021-07-20 11:42:44 -0300

    Criando entidade Comentário (Commentary)

--- src/main/java/com/example/models/CommentaryModel.java
+++ src/main/java/com/example/models/CommentaryModel.java
@@ -1,5 +1,32 @@
 package com.example.models;
 
-public class CommentaryModel {
+public class CommentaryModel extends GenericEntity {
+  private int commentaryId; 
+  private String authorName; 
+  private String commentaryText;
   
+  
+  public int getCommentaryId() {
+    return this.commentaryId;
+  }
+
+  public void setCommentaryId(int commentaryId ) {
+    this.commentaryId = commentaryId;
+  }
+
+  public String getAuthorName() {
+    return this.authorName;
+  }
+
+  public void setAuthorName(String authorName) {
+    this.authorName = authorName;
+  }
+  
+  public String getCommentaryText() {
+    return this.commentaryText;
+  }
+  
+  public void setCommentaryText(String commentaryText) {
+    this.commentaryText = commentaryText;
+  }
 }
