5db17082e93eaee0bc260de7946aa238ea046c97
Lucas Henrique de Assis Monteiro <lucasmonte2011@gmail.com>
2021-07-20 11:58:39 -0300

    Padronização dos nomes dos atributos da classe AlterationModel.

--- src/main/java/com/example/models/AlterationModel.java
+++ src/main/java/com/example/models/AlterationModel.java
@@ -2,36 +2,37 @@ package com.example.models;
 import java.util.Date;
 
 public class AlterationModel extends GenericEntity{
-  public int m_AlterationId;
-  public String m_AlteratorName;
-  public String m_Alteration;
-  public Date m_AlterationDate;
+  private int alterationId;
+  private String alteratorName;
+  private String alteration;
+  private Date alterationDate;
 
-  public int getM_AlterationId() {
-    return m_AlterationId;
+  public int getAlterationId() {
+    return alterationId;
   }
-  public void setM_AlterationId(int m_AlterationId) {
-    this.m_AlterationId = m_AlterationId;
-  }
-
-  public String getM_AlteratorName() {
-    return m_AlteratorName;
-  }
-  public void setM_AlteratorName(String m_AlteratorName) {
-    this.m_AlteratorName = m_AlteratorName;
+  public void setAlterationId(int alterationId) {
+    this.alterationId = alterationId;
   }
 
-  public String getM_Alteration() {
-    return m_Alteration;
+  public String getAlteratorName() {
+    return alteratorName;
   }
-  public void setM_Alteration(String m_Alteration) {
-    this.m_Alteration = m_Alteration;
+  public void setAlteratorName(String alteratorName) {
+    this.alteratorName = alteratorName;
   }
 
-  public Date getM_AlterationDate() {
-    return m_AlterationDate;
+  public String getAlteration() {
+    return alteration;
   }
-  public void setM_AlterationDate(Date m_AlterationDate) {
-    this.m_AlterationDate = m_AlterationDate;
+  public void setAlteration(String alteration) {
+    this.alteration = alteration;
   }
+
+  public Date getAlterationDate() {
+    return alterationDate;
+  }
+  public void setAlterationDate(Date alterationDate) {
+    this.alterationDate = alterationDate;
+  }
+
 }
