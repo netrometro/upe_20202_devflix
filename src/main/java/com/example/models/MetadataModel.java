0963567b495fd7cf728d7e435d89a2a6c9c6f6e5
Kelvin Vasconcelos <kelvinvasconcelos@gmail.com>
2021-07-20 11:18:01 -0300

    Corrigido nomes dos atributos do metadata.

--- src/main/java/com/example/models/MetadataModel.java
+++ src/main/java/com/example/models/MetadataModel.java
@@ -3,58 +3,48 @@ package com.example.models;
 import java.lang.reflect.Array;
 
 public class MetadataModel extends GenericEntity{
-    private int id;
-    private String videoLink;
-    private String title;
-    private String description;
-    private String videoYoutubeChannel;
-    private Array tags;
-
-    public int getId() {
-        return id;
+    private int metadataId;
+    private String metadataVideoLink;
+    private String metadataTitle;
+    private String metadataDescription;
+    private String metadataVideoYoutubeChannel;
+    private Array metadataTags;
+    
+    public int getMetadataId() {
+        return metadataId;
+    }
+    public void setMetadataId(int metadataId) {
+        this.metadataId = metadataId;
+    }
+    public String getMetadataVideoLink() {
+        return metadataVideoLink;
+    }
+    public void setMetadataVideoLink(String metadataVideoLink) {
+        this.metadataVideoLink = metadataVideoLink;
+    }
+    public String getMetadataTitle() {
+        return metadataTitle;
+    }
+    public void setMetadataTitle(String metadataTitle) {
+        this.metadataTitle = metadataTitle;
+    }
+    public String getMetadataDescription() {
+        return metadataDescription;
+    }
+    public void setMetadataDescription(String metadataDescription) {
+        this.metadataDescription = metadataDescription;
+    }
+    public String getMetadataVideoYoutubeChannel() {
+        return metadataVideoYoutubeChannel;
+    }
+    public void setMetadataVideoYoutubeChannel(String metadataVideoYoutubeChannel) {
+        this.metadataVideoYoutubeChannel = metadataVideoYoutubeChannel;
+    }
+    public Array getMetadataTags() {
+        return metadataTags;
+    }
+    public void setMetadataTags(Array metadataTags) {
+        this.metadataTags = metadataTags;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getVideoLink() {
-        return videoLink;
-    }
-
-    public void setVideoLink(String videoLink) {
-        this.videoLink = videoLink;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getVideoYoutubeChannel() {
-        return videoYoutubeChannel;
-    }
-
-    public void setVideoYoutubeChannel(String videoYoutubeChannel) {
-        this.videoYoutubeChannel = videoYoutubeChannel;
-    }
-
-    public Array getTags() {
-        return tags;
-    }
-
-    public void setTags(Array tags) {
-        this.tags = tags;
-    }
 }
