73f1bdc7f74e9856ca28cc8f32c9f635c9741e62
Lucas Henrique de Assis Monteiro <lucasmonte2011@gmail.com>
2021-07-20 11:08:40 -0300

    Implementação dos métodos e atributos da classe AlterationModel

--- src/main/java/com/example/models/AlterationModel.java
+++ src/main/java/com/example/models/AlterationModel.java
@@ -1,5 +1,37 @@
 package com.example.models;
+import java.util.Date;
 
-public class AlterationModel {
-  
+public class AlterationModel extends GenericEntity{
+  public int m_AlterationId;
+  public String m_AlteratorName;
+  public String m_Alteration;
+  public Date m_AlterationDate;
+
+  public int getM_AlterationId() {
+    return m_AlterationId;
+  }
+  public void setM_AlterationId(int m_AlterationId) {
+    this.m_AlterationId = m_AlterationId;
+  }
+
+  public String getM_AlteratorName() {
+    return m_AlteratorName;
+  }
+  public void setM_AlteratorName(String m_AlteratorName) {
+    this.m_AlteratorName = m_AlteratorName;
+  }
+
+  public String getM_Alteration() {
+    return m_Alteration;
+  }
+  public void setM_Alteration(String m_Alteration) {
+    this.m_Alteration = m_Alteration;
+  }
+
+  public Date getM_AlterationDate() {
+    return m_AlterationDate;
+  }
+  public void setM_AlterationDate(Date m_AlterationDate) {
+    this.m_AlterationDate = m_AlterationDate;
+  }
 }
