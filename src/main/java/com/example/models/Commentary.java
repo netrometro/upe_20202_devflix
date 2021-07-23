d83f34eec195848e86d006bfb8924a930e6ff1a9
Luiz Gustavo <luiz.matias@upe.br>
2021-07-23 16:25:36 -0300

    Criando a entidade e dao comentário

---
+++ src/main/java/com/example/models/Commentary.java
@@ -0,0 +1,32 @@
+package com.example.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import org.hibernate.validator.constraints.Length;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@Entity
+@Table(name = "devflix_commentaries")
+public class Commentary {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "commentary_id")
+  private long id;
+
+  @ManyToOne()
+  @JoinColumn(name = "user_id")
+  private User author;
+
+  @Length(max = 256)
+  @Size(max = 126)
+  @NotBlank
+  @NotNull
+  @Column(name = "commentary_text")
+  private String commentaryText; 
+
+}
