3d3449a61e59942bd2c4767215c8fe9c8b38cad2
Lucas Henrique de Assis Monteiro <lucasmonte2011@gmail.com>
2021-07-23 20:20:33 -0300

    Criação da classe Alteration (model) e da interface IAlteration (database).

---
+++ src/main/java/com/example/models/Alteration.java
@@ -0,0 +1,43 @@
+package com.example.models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+@Accessors(chain = true)
+@Entity
+@Table(name = "devflix_alteration")
+public class Alteration {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "alteration_id")
+  private long id;
+
+  @NotNull
+  @NotBlank
+  @Column(name = "alteration_name")
+  private String name;
+  
+  @NotNull
+  @NotBlank
+  @Column(name = "alteration_date")
+  private LocalDateTime date;
+
+  @NotNull
+  @NotBlank
+  @Column(name = "alteration")
+  private String alteration;
+
+}
