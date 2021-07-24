331f2150d2e08b59c9c75ed997dcf6a4d23c4ca0
Lucas Henrique de Assis Monteiro <lucasmonte2011@gmail.com>
2021-07-24 19:42:17 -0300

    Criação do AlterationService.

--- src/main/java/com/example/database/IAlterationDao.java
+++ src/main/java/com/example/database/IAlterationDao.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IAlteration extends JpaRepository<Alteration, Long>{
+public interface IAlterationDao extends JpaRepository<Alteration, Long>{
   
 }
