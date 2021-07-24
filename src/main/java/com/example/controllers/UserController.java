dbf4c8dfd65a5cc1aa86679f314647729349caf0
MurylloEx <muryllopimenta@gmail.com>
2021-07-23 22:40:30 -0300

    Ajuste da rota de Users e Metadata.

--- src/main/java/com/example/controllers/UserController.java
+++ src/main/java/com/example/controllers/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/users")
+@RequestMapping("/v1/users")
 @RestController
 public class UserController {
   
