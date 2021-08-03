66188b47de2f42b05f38d246a9a7bb9978e4bd2a
MurylloEx <muryllopimenta@gmail.com>
2021-08-03 16:05:35 -0300

    Correção de launch.json do .vscode

--- src/main/java/br/upe/devflix/database/IAlterationDao.java
+++ src/main/java/br/upe/devflix/database/IAlterationDao.java
@@ -2,10 +2,11 @@ package br.upe.devflix.database;
 
 import br.upe.devflix.models.Alteration;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+@Component
 @Repository
 public interface IAlterationDao extends JpaRepository<Alteration, Long>{
   
--- src/main/java/br/upe/devflix/database/IMetadataDao.java
+++ src/main/java/br/upe/devflix/database/IMetadataDao.java
@@ -3,8 +3,10 @@ package br.upe.devflix.database;
 import br.upe.devflix.models.Metadata;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
+@Component
 @Repository
 public interface IMetadataDao extends JpaRepository<Metadata, Long>{
   
