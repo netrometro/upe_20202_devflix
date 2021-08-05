05edcc8d979aab0857c49a70d804053aafa84d33
Lucas Henrique de Assis Monteiro <lucasmonte2011@gmail.com>
2021-08-05 11:31:50 -0300

    :sparkles: feature: Finalização da implementação do YouTubeService
    Foram feitos ajustes nos métodos da classe e alguns testes para validar se estavam funcionando corretamente.

--- src/main/java/br/upe/devflix/services/YouTubeService.java
+++ src/main/java/br/upe/devflix/services/YouTubeService.java
@@ -1,6 +1,7 @@
 package br.upe.devflix.services;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -13,18 +14,20 @@ import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
 import com.google.api.services.youtube.model.Video;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class YouTubeService {
-
-
+  //Conecta com a API
   private static YouTube youtube = 
       new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), new HttpRequestInitializer() {
         public void initialize(HttpRequest request) throws IOException {
         }
       }).setApplicationName("Devflix").build();
-
+  //Chave da api
   private static final String API_KEY = "AIzaSyB7CFY0eRyt5NDINZfb0U5A_Ikl3xbOoCg";
-  private static final long NUMBER_OF_VIDEOS_RETURNED = 10;
-
+  //Número de vídeos que serão retornaos na busca
+  private static final long NUMBER_OF_VIDEOS_RETURNED = 15;
 
   //Retorna os metadados do vídeo a partir do seu ID;
   public HashMap<String, String> getVideoMetadata(String videoId) throws IOException {
@@ -40,24 +43,34 @@ public class YouTubeService {
     metadata.put("url", getVideoUrl(videoId));
     metadata.put("title", video.getSnippet().getTitle());
     metadata.put("description", video.getSnippet().getDescription());
-    metadata.put("thumb", video.getSnippet().getThumbnails().getDefault().getUrl());
+    metadata.put("thumb", video.getSnippet().getThumbnails().getHigh().getUrl());
     return metadata;
   }
   //Pesquisa os vídeos a partir de uma palavra-chave e retorna uma lista com vídeos relacionados e seus respectivos metadados
-  public List<SearchResult> getVideoSearch(String query) throws IOException {
+  public List<HashMap<String, String>> getVideoSearch(String query) throws IOException {
     YouTube.Search.List search = youtube.search().list("id,snippet");
     search.setQ(query);
     search.setKey(API_KEY);
     search.setType("video");
-    search.setFields("items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)");
+    search.setFields("items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/high/url)");
     search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
 
     SearchListResponse searchResponse = search.execute();
     List<SearchResult> searchResultList = searchResponse.getItems();
-
-    return searchResultList;
+    List<HashMap<String,String>> videoMetadataList = new ArrayList<>(); 
+    for (SearchResult video : searchResultList) {
+      HashMap<String, String> metadata = new HashMap<String, String>();
+      metadata.put("id", video.getId().getVideoId());
+      metadata.put("url", getVideoUrl(video.getId().getVideoId()));
+      metadata.put("title", video.getSnippet().getTitle());
+      metadata.put("description", video.getSnippet().getDescription());
+      metadata.put("thumb", video.getSnippet().getThumbnails().getHigh().getUrl());
+      videoMetadataList.add(metadata);
+    }
+    return videoMetadataList;
   }
 
+  //Retorna o valor da url a apartir do ID do vídeo
   public String getVideoUrl(String videoId) {
     return ("https://www.youtube.com/watch?v=" + videoId);
   }
