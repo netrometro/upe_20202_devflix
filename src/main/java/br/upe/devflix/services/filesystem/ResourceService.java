1539e7f5efc37bbfcb9402f3388112399b3f0531
MurylloEx <muryllopimenta@gmail.com>
2021-08-06 18:40:37 -0300

    :bug: fix: Correção de bug na leitura de arquivos.

--- src/main/java/br/upe/devflix/services/filesystem/ResourceService.java
+++ src/main/java/br/upe/devflix/services/filesystem/ResourceService.java
@@ -1,10 +1,8 @@
 package br.upe.devflix.services.filesystem;
 
+import java.io.InputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,20 +15,20 @@ public class ResourceService {
   
   @Autowired private ResourceLoader Resources;
   
-  public File getFile(String filePath){
+  public InputStream getInputStream(String filePath){
     Resource resource = Resources.getResource("classpath:" + filePath);
     try {
-      return resource.getFile();
-    } catch (IOException exception) {
+      return resource.getInputStream();
+    } catch (Exception exception) {
       return null;
     }
   }
 
   public BufferedReader getFileBufferedReader(String filePath){
-    File file = getFile(filePath);
     try {
-      return new BufferedReader(new FileReader(file));
-    } catch (FileNotFoundException exception) {
+      InputStream stream = getInputStream(filePath);
+      return new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+    } catch (Exception exception) {
       return null;
     }
   }
