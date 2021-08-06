a851a947a90d172876ee5354c58b795bccf20d86
MurylloEx <muryllopimenta@gmail.com>
2021-08-06 17:36:07 -0300

    📱 feature: Conclusão de integração de Emails.

--- src/main/java/br/upe/devflix/services/subsystems/MailService.java
+++ src/main/java/br/upe/devflix/services/subsystems/MailService.java
@@ -1,49 +1,59 @@
 package br.upe.devflix.services.subsystems;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.muryllo.jmailer.Mailer;
 import br.com.muryllo.jmailer.MailerResponse;
+import br.upe.devflix.services.filesystem.ResourceService;
 
 @Service
 public class MailService {
 
+  @Autowired private ResourceService ResourceProvider;
+
   private Mailer createMailer(){
     return new Mailer()
-      .key("api-C982E608F0D711EB8499F23C91C88F4E")
+      .key("api-D87E11D6F0D311EBA990F23C91C88F4E")
       .from("Devflix UPE", "noreply@upedevflix.herokuapp.com");
   }
 
   public boolean sendMailConfirmation(String userName, String userEmail, String link){
+    String content = ResourceProvider.getFileAllText("mails/welcome.html");
     Mailer mail = createMailer()
       .subject("[DevFlix] Confirmação de Cadastro")
       .to(userName, userEmail)
-      .htmlBody(String.format(
-        "Seja bem vindo ao DevFlix, %s! Para concluir seu " + 
-        "cadastro, clique <a href=\"%s\">neste link</a>.", userName, link));
+      .htmlBody(content
+        .replace("{{DEVFLIX_USERNAME}}", userName)
+        .replace("{{DEVFLIX_USEREMAIL}}", userEmail)
+        .replace("{{DEVFLIX_CONFIRMATIONURL}}", link));
     
     MailerResponse result = mail.send();
     return result.success();
   }
 
-  public boolean sendMailPasswordRecovery(String userName, String userEmail, String code){
+  public boolean sendMailPasswordRecovery(String userName, String userEmail, String recoveryToken){
+    String content = ResourceProvider.getFileAllText("mails/recovery.html");
     Mailer mail = createMailer()
       .subject("[DevFlix] Recuperação de Conta")
       .to(userName, userEmail)
-      .htmlBody(String.format(
-        "Olá %s, recebemos a triste notícia de que você esqueceu sua senha. " +
-        "Mas não se preocupe, você poderá alterá-la utilizando este código: <b>%s</b> ." +
-        "Caso este email não tenha sido solicitado por você, basta ignorá-lo.", userName, code));
+      .htmlBody(content
+        .replace("{{DEVFLIX_USERNAME}}", userName)
+        .replace("{{DEVFLIX_USEREMAIL}}", userEmail)
+        .replace("{{DEVFLIX_RECOVERYTOKEN}}", recoveryToken));
     
     MailerResponse result = mail.send();
     return result.success();
   }
 
-  public boolean sendMailNotification(String userName, String userEmail, String htmlText){
+  public boolean sendMailShareLink(String userName, String userEmail, String link){
+    String content = ResourceProvider.getFileAllText("mails/notification.html");
     Mailer mail = createMailer()
       .subject("[DevFlix] Você possui uma nova notificação.")
       .to(userName, userEmail)
-      .htmlBody(htmlText);
+      .htmlBody(content
+        .replace("{{DEVFLIX_USERNAME}}", userName)
+        .replace("{{DEVFLIX_SHARELINK}}", link));
     
     MailerResponse result = mail.send();
     return result.success();
