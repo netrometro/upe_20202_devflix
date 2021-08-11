e8bf0999de6fe00e3dadb37c1478497c84fbb304
Luiz Gustavo <luiz.matias@upe.br>
2021-08-11 20:20:26 -0300

    feat: Removendo desnecessárias configurações e dependencias

--- web/next.config.js
+++ web/next.config.js
@@ -1,8 +1,5 @@
 const withOptimizedImages = require('next-optimized-images')
 
 module.exports = withOptimizedImages({
-  images: {
-    disableStaticImages: true,
-  },
   handleImages: ['jpeg', 'png', 'svg'],
 })
