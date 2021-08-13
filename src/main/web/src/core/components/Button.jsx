eac8c72648e61e90f6a655fa250bb8c52a46c854
Luiz Gustavo <luiz.matias@upe.br>
2021-08-13 18:25:42 -0300

    refactor(core/components/button): use disabledDefaultBg from utils constants

--- src/main/web/src/core/components/Button.jsx
+++ src/main/web/src/core/components/Button.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Button as ChakraButton, ButtonGroup} from '@chakra-ui/react'
+import {Constants} from 'core/utils'
 
 const Button = ({children, ...props}) => {
-  const disabledBgDefault = {bg: null}
+  const {disabledDefaultBg} = Constants
 
   const {
-    _hover = disabledBgDefault,
-    _focus = disabledBgDefault,
-    _active = disabledBgDefault,
+    _hover = disabledDefaultBg,
+    _focus = disabledDefaultBg,
+    _active = disabledDefaultBg,
   } = props
 
   return (
