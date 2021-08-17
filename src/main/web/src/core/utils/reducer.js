56ea87cd12b95eb195b630d0d5c572fd947ba322
Luiz Gustavo <luiz.matias@upe.br>
2021-08-17 09:22:54 -0300

    feat(core/utils/reducer): create function to use reducer actions methods

---
+++ src/main/web/src/core/utils/reducer.js
@@ -0,0 +1,17 @@
+export default function bindActionCreators(actions, dispatch) {
+  const bindedActions = {}
+
+  for (const action in actions) {
+    if (!Object.prototype.hasOwnProperty.call(actions, action)) {
+      continue
+    }
+
+    if (typeof actions[action] !== 'function') {
+      throw Error('Action creator must be a function')
+    }
+
+    bindedActions[action] = (...args) => dispatch(actions[action](...args))
+  }
+
+  return bindedActions
+}
