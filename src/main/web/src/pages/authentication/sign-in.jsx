2e3ff6381a36d654649cb2363d37f7043e18ace8
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-08-17 11:38:10 -0300

    :sparkles: Criando tela de login.

--- src/main/web/src/pages/authentication/sign-in.jsx
+++ src/main/web/src/pages/authentication/sign-in.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
+import {Box, Center, Container} from '@chakra-ui/react'
+import {Image, Button} from 'core/components'
+import {EmailIcon, LockIcon} from '@chakra-ui/icons'
+import FormField from 'core/components/Form/FormField'
+
+const FORMFIELDS = [
+  {type: "email", icon: <EmailIcon />, text: 'Email'},
+  {type: "psw", icon: <LockIcon />, text: 'Senha'}
+]
 
 const SignIn = () => {
   return (
-    <div>
-      <h1>Tela de login</h1>
-    </div>
+    <Box className='box' bg="background" py={20} alignItems="center" width={'100%'}>
+      <Center>
+        <Image
+          src={require('images/logo.svg')}
+          alt="logo"
+          width={500}
+          height={125}
+          mb={5}
+          display='inline-block'
+        />
+      </Center>  
+      <Container>
+        {FORMFIELDS.map((formfield, index) => {
+          return (
+            <form key={`${index}`}> 
+              <FormField {...formfield}/>
+            </form>
+          )
+        })}
+      </Container>
+      <Center>
+        <Button size='lg'>
+          Entrar
+        </Button>
+      </Center>
+      <Center>
+        <Button color="#A8A5A5" variant="ghost" mt={'20px'} size='lg'>
+          Esqueceu a senha?
+        </Button>
+      </Center>
+      <Center>
+        <Box py={0.5} px={3} width={'20%'} bg='primary' borderRadius={1} mt={5} mb={10}/>
+      </Center>
+      <Center>
+        <Button color="primary" borderColor="primary" variant="outline" size='lg'>
+          Realizar Cadastro
+        </Button>
+      </Center>
+    </Box>
   )
 }
 
