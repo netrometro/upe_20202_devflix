a1d292aad461776f7f190db5e2c28e6c6cc2338a
MurylloEx <muryllopimenta@gmail.com>
2021-08-18 16:45:56 -0300

    :sparkles: refactor: Remoção de métodos desnecessários.
    No MetadataController haviam métodos desnecessários que foram removidos pois não serão utilizados.

--- src/main/java/br/upe/devflix/controllers/MetadataController.java
+++ src/main/java/br/upe/devflix/controllers/MetadataController.java
@@ -1,7 +1,5 @@
 package br.upe.devflix.controllers;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
 import br.upe.devflix.models.entities.*;
@@ -9,10 +7,8 @@ import br.upe.devflix.models.entities.*;
 
 import org.springframework.http.ResponseEntity;
 //import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,27 +20,12 @@ public class MetadataController {
 
   //@Autowired private MetadataCRUDService metadataService;
 
-  @GetMapping
-  public ResponseEntity<List<Metadata>> fetchAll()
-  {
-    return null;
-    //return metadataService.fetchAll();
-  }
-
   @GetMapping("/{metadaId}")
   public ResponseEntity<Metadata> fetch(
     @PathVariable Long metadaId)
   {
     return null;
     //return metadataService.fetch(metadaId);
-  }
-
-  @PostMapping
-  public ResponseEntity<Metadata> create(
-    @RequestBody @Valid Metadata metadata)
-  {
-    return null;
-    //return metadataService.create(metadata);
   }
 
   @PutMapping("/{metadataId}")
@@ -56,11 +37,4 @@ public class MetadataController {
     //return metadataService.update(metadataId, metadata);
   }
 
-  @DeleteMapping("/{metadataId}")
-  public ResponseEntity<Metadata> delete(
-    @PathVariable Long metadataId)
-  {
-    return null;
-    //return metadataService.delete(metadataId);
-  }
 }
