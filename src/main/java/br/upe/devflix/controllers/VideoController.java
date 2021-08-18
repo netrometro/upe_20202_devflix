ed3928d43347a69c342dce92b9701122f220b253
MurylloEx <muryllopimenta@gmail.com>
2021-08-18 17:46:00 -0300

    :sparkles: feature: Criação de rota de pesquisa no youtube.
    Agora é possível pesquisar por vídeos no Youtube a partir do DevFlix, utilizando a rota /api/v1/video/youtube/search?keyword=<palavra-chave>.

--- src/main/java/br/upe/devflix/controllers/VideoController.java
+++ src/main/java/br/upe/devflix/controllers/VideoController.java
@@ -1,25 +1,28 @@
 package br.upe.devflix.controllers;
 
-import java.util.List;
-
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import java.util.HashMap;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import br.upe.devflix.models.entities.Category;
 import br.upe.devflix.models.entities.Video;
 import br.upe.devflix.services.CategoryCRUDService;
 import br.upe.devflix.services.VideoCRUDService;
 import br.upe.devflix.services.serializers.ResponseService;
+import br.upe.devflix.services.subsystems.YouTubeService;
 
 @RequestMapping("/api/v1/video")
 @RestController
@@ -28,6 +31,15 @@ public class VideoController {
   @Autowired private ResponseService responseService;
   @Autowired private VideoCRUDService videoService;
   @Autowired private CategoryCRUDService categoryService;
+  @Autowired private YouTubeService youtubeService;
+
+  @GetMapping("/youtube/search")
+  public ResponseEntity<?> searchByKeyword(
+    @RequestParam("keyword") String keyword)
+  {
+    List<HashMap<String, String>> youtubeVideos = youtubeService.getVideoSearch(keyword);
+    return responseService.create(youtubeVideos, HttpStatus.OK);
+  }
 
   @GetMapping
   public ResponseEntity<?> fetchAll() {
