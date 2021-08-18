0fc5705dfa0a86b9fb6c0985f55afeb4c8ec11c4
MurylloEx <muryllopimenta@gmail.com>
2021-08-18 19:50:42 -0300

    :recycle: refactor: Alterando nomes de campos de entidades

--- src/main/java/br/upe/devflix/models/entities/Video.java
+++ src/main/java/br/upe/devflix/models/entities/Video.java
@@ -36,18 +36,18 @@ public class Video extends GenericEntity {
   @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "video_id")
   private long id;
-
-  @OneToOne
-  private Metadata videoMetadata;
   
   @Min(1)
   @Max(2)
   @NotNull
   @Column(name = "video_visibility")
-  private int videoVisibility = 1;
+  private int visibility = 1;
+
+  @OneToOne
+  private Metadata metadata;
   
   @OneToMany(mappedBy = "video")
-  private List<Commentary> videoComments;
+  private List<Commentary> commentaries;
   
   @JsonIgnore
   @ManyToOne(fetch = FetchType.LAZY, optional = false)
