fdfb05526ad9e0ade80dbb8902aecbf97d044a18
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-08-19 23:39:33 -0300

    :construction: feature: Implementeado verificação de autenticação.

--- src/main/java/br/upe/devflix/controllers/ShareController.java
+++ src/main/java/br/upe/devflix/controllers/ShareController.java
@@ -7,6 +7,7 @@ import br.upe.devflix.services.serializers.ResponseService;
 import br.upe.devflix.services.subsystems.MailService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,6 +30,9 @@ public class ShareController {
     @RequestHeader("authorization") String authorization,
     @RequestBody @Valid ShareContentDTO content) 
   {
+    if (!authorizationService.isAuthenticated(authorization)){
+      return responseService.create(null, HttpStatus.FORBIDDEN);
+    }
     return null;
   }
 }
