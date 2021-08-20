42692ff08bc924fd81d0d4e9a08fd66a6368f892
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-08-19 23:31:27 -0300

    :construction: feature: Criado rota de compartilhamento.

--- src/main/java/br/upe/devflix/controllers/ShareController.java
+++ src/main/java/br/upe/devflix/controllers/ShareController.java
@@ -1,13 +1,24 @@
 package br.upe.devflix.controllers;
 
+import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.upe.devflix.models.dto.ShareContentDTO;
 
 @RequestMapping("/api/v1/share")
 @RestController
 public class ShareController {
   
- 
+  @PostMapping
+  public ResponseEntity<?> shareLink(
+    @RequestHeader("authorization") String authorization,
+    @RequestBody @Valid ShareContentDTO content) 
+  {
+    return null;
+  }
 }
