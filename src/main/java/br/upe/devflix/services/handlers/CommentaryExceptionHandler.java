4301b5b9402554cfdd053eda6b8adf6ea1f4163c
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-08-20 22:43:33 -0300

    :heavy_plus_sign: feature: Adicionado handler para exceção nos comentarios.

---
+++ src/main/java/br/upe/devflix/services/handlers/CommentaryExceptionHandler.java
@@ -0,0 +1,37 @@
+package br.upe.devflix.services.handlers;
+
+import java.time.LocalDateTime;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import br.upe.devflix.base.exceptions.CommentaryNotFoundException;
+import br.upe.devflix.models.dto.ExceptionResponseDTO;
+
+@Slf4j
+@RestControllerAdvice
+public class CommentaryExceptionHandler {
+  
+  @ExceptionHandler(CommentaryNotFoundException.class)
+  @ResponseStatus(value = HttpStatus.NOT_FOUND)
+  protected ExceptionResponseDTO commentaryNotFound(
+    Exception exception,
+    WebRequest request) 
+  {
+    log.warn("Error, commentary not found.", exception);
+
+    ExceptionResponseDTO response = new ExceptionResponseDTO()
+      .setTitle(exception.getMessage())
+      .setStatus(HttpStatus.NOT_FOUND.value())
+      .setError(true)
+      .setTimestamp(LocalDateTime.now());
+
+    return response;
+  }
+
+}
