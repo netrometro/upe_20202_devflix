4a97e1e2bf4772b15f36315c37dd24010cfb95c0
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-08-28 17:40:53 -0300

    :sparkles: feature: Finalizado modal de minhas categorias.

--- src/main/web/src/core/components/Category/Title.jsx
+++ src/main/web/src/core/components/Category/Title.jsx
@@ -1,4 +1,4 @@
-import {Box, Flex} from '@chakra-ui/react'
+import {Box, Flex, HStack} from '@chakra-ui/react'
 import React from 'react'
 import Text from '../Text'
 import DeleteIcon from '@material-ui/icons/Delete';
@@ -11,8 +11,12 @@ const Title = ({text, color, deleteIcon, editIcon}) => {
       <Text color="secondary" fontSize={40} fontWeight={700}>
         {text}
       </Text>
-      {deleteIcon && <DeleteIcon></DeleteIcon>}
-      {editIcon && <EditIcon></EditIcon>}
+      <HStack>
+        <Box mt="1" ml="3">
+          {deleteIcon && <DeleteIcon style={{ color: "#BDBDBD", marginRight: "3", fontSize: "38px"}}></DeleteIcon>}
+          {editIcon && <EditIcon style={{ color: "#BDBDBD", marginLeft: "3", fontSize: "38px"}}></EditIcon>}
+        </Box>
+      </HStack>
     </Flex>
   )
 }
