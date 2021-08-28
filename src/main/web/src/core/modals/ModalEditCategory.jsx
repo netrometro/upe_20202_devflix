ae5964d2aec4054c0b9d3f5702dc552747e440aa
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-08-28 18:00:43 -0300

    :construction: Criado estrutura de editar categoria.

---
+++ src/main/web/src/core/modals/ModalEditCategory.jsx
@@ -0,0 +1,49 @@
+import {React} from 'react'
+import {Button, Text, Input, Select, HStack, useDisclosure} from "@chakra-ui/react"
+import {Modal} from "core/components"
+import {ModalMyCategories} from 'core/modals'
+
+const ModalEditCategory = ({...props}) => {
+  const { isOpen: isMyCategoriesOpen , onOpen: onMyCategoriesOpen, onClose: onMyCategoriesClose } = useDisclosure()
+  const header = ({title, ...props}) => {
+    return(
+      <Text {...props} color="whiteLight" fontSize="32px">{title}</Text>
+    )
+  }
+
+  return(
+    <Modal 
+      header={header({title : "Cadastro de Categoria"})}
+      scrollBehavior="inside" 
+      {...props}
+      >
+      <Input w="65%" ml="5px" mt="10px" variant="flushed" color="whiteLight" _placeholder={{ color: 'whiteLight' }} borderColor="primary" focusBorderColor="primary" placeholder="Título" />
+      <Input w="65%" ml="5px" mt="10px" variant="flushed" color="whiteLight" _placeholder={{ color: 'whiteLight' }} borderColor="primary" focusBorderColor="primary" placeholder="Cor" />
+      <Select w="65%" ml="5px" mt="10px" variant="flushed" color="whiteLight" _placeholder={{ color: 'whiteLight' }} borderColor="primary" focusBorderColor="primary" placeholder="Selecione a Visibilidade" />
+        
+      <HStack
+      mt="5%"
+      spacing="5%"> 
+        <Button
+          size="lg"
+          >
+            Cadastrar
+        </Button>
+        <ModalMyCategories isOpen={isMyCategoriesOpen} onClose={onMyCategoriesClose}></ModalMyCategories>
+        <Button
+          onClick={onMyCategoriesOpen}
+          _hover={{ bg: "#EC00254F" }}
+          color="primary"
+          borderColor="primary"
+          variant="outline"
+          size="lg">
+          Minhas Categorias
+        </Button>
+      </HStack>
+      
+    </Modal>
+  )
+  
+}
+
+export default ModalEditCategory
