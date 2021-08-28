58f968233876004f3adaa9930ba6afafd97db8d0
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-08-28 17:24:40 -0300

    :construction: Criado estrutura do modal de minhas categorias.

--- src/main/web/src/core/components/Category/Title.jsx
+++ src/main/web/src/core/components/Category/Title.jsx
@@ -1,14 +1,18 @@
 import {Box, Flex} from '@chakra-ui/react'
 import React from 'react'
 import Text from '../Text'
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
-const Title = ({text, color}) => {
+const Title = ({text, color, deleteIcon, editIcon}) => {
   return (
     <Flex pl={35} flexDirection="row">
       <Box width={4} bg={color} mr={4} />
       <Text color="secondary" fontSize={40} fontWeight={700}>
         {text}
       </Text>
+      {deleteIcon && <DeleteIcon></DeleteIcon>}
+      {editIcon && <EditIcon></EditIcon>}
     </Flex>
   )
 }
