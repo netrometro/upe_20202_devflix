b9d3a868f4f435c6f9bc43e4d66f0c58e85f20d3
Lucas Henrique <lucasmonte2011@gmail.com>
2021-08-27 21:04:49 -0300

    :sparkles: feature: Criando componente genérico Modal

--- src/main/web/src/core/components/Modal.jsx
+++ src/main/web/src/core/components/Modal.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import {
+  Modal as ChakraModal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 
-// import { Container } from './styles';
+const Modal = ({
+  isOpen,
+  onClose,
+  header,
+  children,
+  footer,
+  bg = "background",
+  ...props
+}) => {
+  return (
+    <>
+      <ChakraModal
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered
+        m={100}
+        {...props}
+      >
+        <ModalOverlay />
+        <ModalContent bg={bg}>
+          <ModalHeader>{header}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>{children}</ModalBody>
+          <ModalFooter>{footer}</ModalFooter>
+        </ModalContent>
+      </ChakraModal>
+    </>
+  );
+};
 
-const Modal = () => {
-  return <div />
-}
+Modal.ModalOverlay = ModalOverlay;
+Modal.Content = ModalContent;
+Modal.Header = ModalHeader;
+Modal.CloseButton = ModalCloseButton;
+Modal.Body = ModalBody;
+Modal.Footer = ModalFooter;
 
-export default Modal
+export default Modal;
