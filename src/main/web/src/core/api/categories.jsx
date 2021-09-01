690426af9559db9c108bf85c4b81aeb690ae7150
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-09-01 15:35:10 -0300

    :sparkles: feature: Criado requisições de metadados.

--- src/main/web/src/core/api/categories.jsx
+++ src/main/web/src/core/api/categories.jsx
@@ -1,22 +1,22 @@
 const { default: useToken } = require("core/hooks/useToken");
 const { get, post, del, put } = require("./requests");
 
-function getCategories(response){
+export function getCategories(response){
   get('/category', null, null, response);
 }
 
-function getCategoryById(id, response){
+export function getCategoryById(id, response){
   get('/category/' + encodeURIComponent(id), null, null, response);
 }
 
-function addCategory({title, color, visibility}, response){
+export function addCategory({title, color, visibility}, response){
   post('/category', null, {title, color, visibility}, useToken(), response);
 }
 
-function editCategory(id, {title, color, visibility}, response){
+export function editCategory(id, {title, color, visibility}, response){
   put('/category/' + encodeURIComponent(id), null, {title, color, visibility}, useToken(), response);
 }
 
-function delCategory(id, response){
+export function delCategory(id, response){
   del('/category/' + encodeURIComponent(id), null, useToken(), response);
 }
