8ce95bc9e7a87a88c63ae6af1d2cca65cbe0e783
Muryllo <muryllopimenta@gmail.com>
2021-09-01 15:04:14 -0300

    :sparkles: feature: Modelo de integração das requisições.

---
+++ src/main/web/src/core/api/categories.jsx
@@ -0,0 +1,23 @@
+const { default: useToken } = require("core/hooks/useToken");
+const { get, post, del, put } = require("./requests");
+
+
+function getCategories(response){
+  get('/category', null, null, response);
+}
+
+function getCategoryById(id, response){
+  get('/category/' + encodeURIComponent(id), null, null, response);
+}
+
+function addCategory({title, color, visibility}, response){
+  post('/category', null, {title, color, visibility}, useToken(), response);
+}
+
+function delCategory(id, response){
+  del('/category/' + encodeURIComponent(id), null, useToken(), response);
+}
+
+function editCategory(id, {title, color, visibility}, response){
+  put('/category/' + encodeURIComponent(id), null, {title, color, visibility}, useToken(), response);
+}
--- src/main/web/src/pages/index.jsx
+++ src/main/web/src/pages/index.jsx
@@ -3,6 +3,8 @@ import {Box} from '@chakra-ui/react'
 import {Category, Navbar} from 'core/components'
 import {PagesTitles} from 'core/utils/constants'
 import {useUser} from 'core/hooks'
+import { get } from 'core/api/requests'
+
 
 const CATEGORIES = [
   {color: 'green', title: 'Back end'},
@@ -14,6 +16,11 @@ const HomePage = () => {
   const [state, actions] = useUser()
 
   console.log({state, actions})
+
+  get('/api/v1/category', 
+    null, null, function(data, status){
+    console.log(data, status)
+  });
 
   return (
     <>
