2ce7557affb1069c9592cd83e524995949ed7c3b
Lucas Henrique <lucasmonte2011@gmail.com>
2021-09-04 02:04:35 -0300

    :sparkles: feature: Integrando edição de informações no  ModalUser

--- src/main/web/src/core/modals/ModalUser.jsx
+++ src/main/web/src/core/modals/ModalUser.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
   Button,
   Text,
@@ -6,16 +6,57 @@ import {
   HStack,
   InputGroup,
   InputRightElement,
+  IconButton,
+  VStack
 } from '@chakra-ui/react'
 import {Modal} from 'core/components'
 import PersonIcon from '@material-ui/icons/Person'
 import EmailIcon from '@material-ui/icons/Email'
 import EditIcon from '@material-ui/icons/Edit'
 import { useUser } from 'core/hooks'
+import usePutRequest from 'core/hooks/usePutRequest'
+import Alert from 'core/components/Alert'
+
 
 const ModalUser = ({...props}) => {
-  const [{ name: userName, email: userEmail }] = useUser()
-  console.log(userName)
+  const [{ name: userName, email: userEmail, id: userId }] = useUser()
+
+  const [actualUserName, setActualUserName] = useState(userName)
+  const [actualUserEmail, setActualUserEmail] = useState(userEmail)
+
+  const [isEditUserNameAvailable, setIsEditUserNameAvailable] = useState(true)
+  const [isEditUserEmailAvailable, setIsEditUserEmailAvailable] = useState(true)
+
+  const {
+    mutate: editUser,
+    isLoading,
+    isSuccess,
+    isError,
+  } = usePutRequest(`/v1/user/${userId}`);
+
+  const onClickCreateUser = () => {
+    editUser({
+      name: actualUserName,
+      email: actualUserEmail
+    });
+  };
+
+  const renderAlert = () => {
+    const error = {
+      status: 'error',
+      body: 'Eita! Ocorreu um erro ao processar a sua solicitação de login. Por favor, cheque as suas credenciais de acesso e tente novamente!',
+    }
+    const success = {
+      status: 'success',
+      body: 'Login realizado com sucesso!',
+    }
+    const buildMessage = () => {
+      return isError ? error : success
+    }
+
+    const {status, body} = buildMessage()
+    return (isError || isSuccess) && <Alert status={status} message={body} />
+  }
 
   const header = ({title, ...props}) => {
     return (
@@ -44,11 +85,15 @@ const ModalUser = ({...props}) => {
             _placeholder={{color: 'whiteLight'}}
             borderColor="primary"
             focusBorderColor="primary"
-            placeholder={userName}
+            onChange={(event) => setActualUserName(event.target.value)}
+            value={actualUserName??userName}
+            isDisabled={isEditUserNameAvailable}
           />
           <InputRightElement>
-            <EditIcon
-              style={{color: '#BDBDBD', marginLeft: '3', fontSize: '38px'}}
+            <IconButton
+            backgroundColor="background"
+            onClick={() => setIsEditUserNameAvailable(false)}
+            icon={<EditIcon style={{color: '#BDBDBD', marginLeft: '3', fontSize: '38px'}}/>}
             />
           </InputRightElement>
         </InputGroup>
@@ -67,17 +112,25 @@ const ModalUser = ({...props}) => {
             _placeholder={{color: 'whiteLight'}}
             borderColor="primary"
             focusBorderColor="primary"
-            placeholder={userEmail}
+            onChange={(event) => setActualUserEmail(event.target.value)}
+            value={actualUserEmail??userEmail}
+            isDisabled={isEditUserEmailAvailable}
           />
           <InputRightElement>
-            <EditIcon
-              style={{color: '#BDBDBD', marginLeft: '3', fontSize: '38px'}}
+            <IconButton
+            backgroundColor="background"
+            onClick={() => setIsEditUserEmailAvailable(false)}
+            icon={<EditIcon style={{color: '#BDBDBD', marginLeft: '3', fontSize: '38px'}}/>}
             />
+            
           </InputRightElement>
         </InputGroup>
       </HStack>
-      <HStack mt="5%" spacing="5%">
-        <Button size="lg">Confirmar</Button>
+      <VStack maxWidth="600px" py="4">
+        {renderAlert()}
+      </VStack>
+      <HStack spacing="5%">
+        <Button size="lg" onClick={onClickCreateUser} isLoading={isLoading}>Confirmar</Button>
       </HStack>
     </Modal>
   )
