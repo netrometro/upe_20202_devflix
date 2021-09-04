4002d133f01b17d8f79562dcc7163c1614caeaee
Lucas Henrique <lucasmonte2011@gmail.com>
2021-09-04 02:01:54 -0300

    :adhesive_bandage: fix: corrigindo exports

--- src/main/web/src/core/hooks/index.jsx
+++ src/main/web/src/core/hooks/index.jsx
@@ -8,5 +8,6 @@ export {default as useStorage} from './useStorage'
 export {default as useGetAllVideos} from './videos/useGetAllVideos'
 export {default as useAddVideo} from './videos/useAddVideo'
 export {default as useGetAllCategories} from './useGetAllCategories'
+export {default as useToken} from './useToken'
 
 export * from './authentication'
