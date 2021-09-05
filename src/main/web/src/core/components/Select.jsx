4aa93b95d0a0bfb81ba895a05feb763f33001219
KelvinVasconcelos <kelvinvasconcelos2001@gmail.com>
2021-09-04 23:03:17 -0300

    :art: Corrigido cor de fundo dos selects.

--- src/main/web/src/core/components/Select.jsx
+++ src/main/web/src/core/components/Select.jsx
@@ -1,8 +1,10 @@
 import {Select as ChakraSelect, Text, Stack} from '@chakra-ui/react'
+import { useTheme } from 'core/hooks';
 
 const Select = ({items, defaultValue, label, ...props}) => {
+  const {colors} = useTheme();
   const renderOptionElement = ({value, label}, index) => (
-    <option value={value} key={index}>
+    <option value={value} key={index} style={{backgroundColor: colors.background}} >
       {label}
     </option>
   )
