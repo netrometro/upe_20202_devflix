ca80ac77cb5b4bf2ad0dde85a31cb5f86231c024
Lucas Henrique <lucasmonte2011@gmail.com>
2021-09-07 01:31:15 -0300

    :sparkles: feature: possibilitando alteração de visibilidade de vídeo

--- src/main/web/src/core/modals/ModalEditVideo.jsx
+++ src/main/web/src/core/modals/ModalEditVideo.jsx
@@ -1,18 +1,24 @@
 import React, {useState, useEffect} from 'react'
-import {Modal, Alert} from "core/components"
+import {Modal, Alert, Select} from "core/components"
 import {Button, Text, Input, VStack, Textarea} from "@chakra-ui/react"
 import usePutRequest from 'core/hooks/usePutRequest'
 import { useRouter } from "next/router";
 
+const OPTIONS_VISIBILITY = [
+  {label: 'Público', value: 1},
+  {label: 'Privado', value: 2},
+]
+
 const ModalEditVideo = ({video, ...props}) => {
 
-  const {metadata, id, visibility} = video
+  const {metadata, id} = video
 
   const {videoLink, description, videoYoutubeChannel, title, videoThumbnail, tags} = metadata ?? {}
 
   const [actualDescription, setActualDescription] = useState(description)
   const [actualTitle, setActualTitle] = useState(title)
   const [actualTags, setActualTags] = useState(tags)
+  const [visibility, setVisibility] = useState(1)
 
   const router = useRouter();
 
@@ -78,6 +84,19 @@ const ModalEditVideo = ({video, ...props}) => {
         <Input w="65%" ml="5px" mt="10px" variant="flushed" color="whiteLight" _placeholder={{ color: 'whiteLight' }} borderColor="primary" focusBorderColor="primary" placeholder="Título" onChange={(event) => setActualTitle(event.target.value)}/>
         <Textarea w="65%" ml="5px" mt="10px" variant="flushed" color="whiteLight" _placeholder={{ color: 'whiteLight' }} borderColor="primary" focusBorderColor="primary" placeholder="Descrição" onChange={(event) => setActualDescription(event.target.value)}/>
         <Input w="65%" ml="5px" mt="10px" variant="flushed" color="whiteLight" _placeholder={{ color: 'whiteLight' }} borderColor="primary" focusBorderColor="primary" placeholder="Tags" onChange={(event) => setActualTags(event.target.value)}/>
+        <Select
+          w="65%"
+          ml="5px"
+          mt="10px"
+          variant="flushed"
+          color="whiteLight"
+          _placeholder={{color: 'whiteLight'}}
+          borderColor="primary"
+          focusBorderColor="primary"
+          placeholder="Selecione a Visibilidade"
+          items={OPTIONS_VISIBILITY}
+          onChange={(event) => setVisibility(event.target.value)}
+        />
         <VStack mt={10}>
           {renderAlert()}
         </VStack>
