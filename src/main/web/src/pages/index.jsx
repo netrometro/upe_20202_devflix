236a567d4f743847d49beee0229523848e4a427e
Luiz Gustavo <luiz.gustavo@clin.digital>
2021-09-07 14:54:57 -0300

    feat: add empty page in home screen

--- src/main/web/src/pages/index.jsx
+++ src/main/web/src/pages/index.jsx
@@ -1,8 +1,9 @@
 import React, {useState, useEffect} from 'react'
-import {Box} from '@chakra-ui/react'
-import {Category, Navbar} from 'core/components'
+import {Box, Center, VStack} from '@chakra-ui/react'
+import {Category, Image, Navbar, Text} from 'core/components'
 import {LOCAL_STORAGES_LOCATIONS, PagesTitles} from 'core/utils/constants'
 import {useGetAllCategories, useStorage, useUser} from 'core/hooks'
+import EmptyImage from 'images/empty.svg'
 
 const HomePage = () => {
   const [{response: categories = [], isLoading}] = useGetAllCategories()
@@ -23,24 +24,42 @@ const HomePage = () => {
     return null
   }, [actualUser, getItem, login, user.email])
 
+  const DefaultHomePage = () => (
+    <Box bg="background" py={20}>
+      {categories.map((category, index, categories) => {
+        const isLastCategory = categories.length - 1 === index
+        const canShowCategory = category.category.visibility === 1
+        return (
+          <>
+            {canShowCategory && (
+              <Box key={`${index}`}>
+                <Category {...category} isLoading={isLoading} />
+                {!isLastCategory && <Box height={20} />}
+              </Box>
+            )}
+          </>
+        )
+      })}
+    </Box>
+  )
+
+  const EmptyHomePage = () => (
+    <Center bg="background" py={20}>
+      <VStack>
+        <Image src={EmptyImage} alt="imagem categorias vazia" />
+        <Text color="primary" fontSize="2xl">
+          Vish! Não tem nenhuma categoria cadastrada. 😞
+        </Text>
+      </VStack>
+    </Center>
+  )
+
+  const HomePage = categories.length === 0 ? EmptyHomePage : DefaultHomePage
+
   return (
     <>
       <Navbar />
-      <Box bg="background" py={20}>
-        {categories.map((category, index, categories) => {
-          const isLastCategory = categories.length - 1 === index
-          const canShowCategory = (category.category.visibility === 1)
-          return (
-            <>
-            {canShowCategory && (
-                <Box key={`${index}`}>
-              <Category {...category} isLoading={isLoading} />
-              {!isLastCategory && <Box height={20} />}
-            </Box>)}
-            </>
-          )
-        })}
-      </Box>
+      <HomePage />
     </>
   )
 }
